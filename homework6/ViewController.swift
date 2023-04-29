5c3933d3450f47ebb51c5fc1f7575264b8c8da6a
Michael Spain <m.spain@setonhill.edu>
2023-04-29 16:52:19 -0400

    Initial Commit

---
+++ homework6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework6
+//
+//  Created by Michael spain on 4/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
